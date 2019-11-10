db867e1add9736e0aaa30e24304154f7dae3279c
Chinj17 <chinmaynj17@gmail.com>
2019-11-09 22:23:44 -0500

    Added the folder containing the testing files

---
+++ test/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file talker.cpp
+ * @brief C++ file for the publisher node
+ *
+ * This file contains the implementation of tutorials according to the
+ * guidelines given in the assignment taken from ROS Wiki
+ *
+ * @copyright Copyright (c) Fall 2019 ENPM808X
+ *            This project is released under the BSD 3-Clause License.
+ *
+ * @author Chinmay Joshi
+ *
+ * @date 11-3-2019
+ */
+
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+
+/**
+* @brief main function which runs all test results.
+*/
+int main(int argc, char** argv) {
+  // Initialize a ROS node
+  ros::init(argc, argv, "beginnerTutorialsTest");
+
+  // Initialize Google Test
+  ::testing::InitGoogleTest(&argc, argv);
+
+  // Run all tests
+  return RUN_ALL_TESTS();
+}
